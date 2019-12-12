38497b9a269f60f67e6abae20565afaa42792c45
Ku <t108598019@ntut.org.tw>
2019-12-12 19:07:41 +0800

    management api

--- app/blueprint/__init__.py
+++ app/blueprint/__init__.py
@@ -4,3 +4,4 @@ from .patient import patient as patient_bp
 from .registration import registration as registration_bp
 from .invoice import invoice as invoice_bp
 from .medication import medication as medication_bp
+from .management import management as management_bp
